68f430c986a816cb358d3aad369d9c34bc80b888
Alexa <alexa.prokofieva@yandex.ru>
2023-09-19 14:14:09 +0300

    Complemented Task55

--- Task55.py
+++ Task55.py
@@ -29,3 +29,58 @@ def find_contact():
         for contact in file:
             if search in contact:
                 list_1.append(contact)
+                
+def change_contact():
+    show_contact()
+    with open(FILE_PATH, 'r+', encoding='utf8') as file:
+        n = int(input('Enter a contact number to change it: '))
+        lines = file.readlines()
+        if n > len(lines):
+            print('Invalid contact number')
+            return
+        new_info = input('Enter data according to the sample - Last name First name; phone number: ')
+        lines[n-1] = f'{n}. {new_info}\n'
+        file.seek(0)
+        file.writelines(lines)             
+        
+def delete_contact():
+    show_contact()
+    with open(FILE_PATH, 'r+', encoding='utf8') as file:
+        n = int(input('Enter a contact number to delete it: '))
+        lines = file.readlines()
+        if n > len(lines):
+            print('Invalid contact number')
+            return
+        del lines[n-1]
+        file.seek(0)
+        file.truncate(0)
+        file.writelines(lines)
+    
+def choose():
+    flag=True
+    while flag:
+        print('Options: \
+        1 - add contact \
+        2 - show contacts \
+        3 - find contact \
+        4 - change contact \
+        5 - delete contact \
+        6 - finish session')
+        n = input('Enter option: ')
+        match n:
+            case '1':
+                add_contact()
+            case '2':
+                show_contact()
+            case '3':
+                find_contact()
+            case '4':
+                change_contact()
+            case '5':
+                delete_contact()
+            case '6':
+                flag=False
+
+choose()
+        
+    
